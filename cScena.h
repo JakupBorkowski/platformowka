355f29fea35427f1a03df3124d3d0732759ef55d
JakupBorkowski <50780955+JakupBorkowski@users.noreply.github.com>
2019-05-21 21:41:03 +0200

    Add files via upload

---
+++ cScena.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "cProstokat.h"
+#include "cOkrag.h"
+#include "cPrzeszkoda.h"
+#include "cPostac.h"
+#include <vector>
+
+//bindingi dla callbacków
+void resize_binding(int width, int height);
+void idle_binding();
+void display_binding();
+void key_binding(unsigned char key, int x, int y);
+void timer_binding(int i);
+void motion_binding(int x, int y);
+
+class cScena {
+	std::vector<cFigura*> figury_;
+	int active_id_;
+public:
+	cScena();
+	~cScena();
+	void resize(int width, int height);
+	void timer();
+	void display();
+	void init(int argc, char *argv[], const char* window_name);
+	void set_callbacks();
+	void key(unsigned char key, int x, int y);
+	void motion(int x, int y);
+
+};
